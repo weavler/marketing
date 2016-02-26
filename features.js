8c2d3868c0f1b8b3136da149d96233e8a33dab9d
Peramanathan <prem@weavler.it>
2016-02-26 12:46:44 +0100

    Marketing timeline first version

---
+++ features.js
@@ -0,0 +1,40 @@
+{
+  "features":[
+    {
+      "title": "Webshop (alpha)",
+      "content": "This version should have following features",
+      "tasks": ["Signin and Signup", "Add and Update Profile", "Add and Update Product"],
+      "date": "March 30, 2016"
+    },
+    {
+      "title": "Webshop (Beta)",
+      "content": "This version should have following features",
+      "tasks": ["Signin and Signup", "Add and Update Profile", "Add and Update Product"],
+      "date": "April 30, 2016"
+    },
+    {
+      "title": "Webshop (v1.0)",
+      "content": "This version should have following features",
+      "tasks": ["Signin and Signup", "Add and Update Profile", "Add and Update Product"],
+      "date": "March 30, 2016"
+    },
+    {
+      "title": "Webshop (v1.0)",
+      "content": "This version should have following features",
+      "tasks": ["Signin and Signup", "Add and Update Profile", "Add and Update Product"],
+      "date": "March 30, 2016"
+    },
+    {
+      "title": "Webshop (v1.0)",
+      "content": "This version should have following features",
+      "tasks": ["Signin and Signup", "Add and Update Profile", "Add and Update Product"],
+      "date": "March 30, 2016"
+    },
+    {
+      "title": "Webshop (v1.0)",
+      "content": "This version should have following features",
+      "tasks": ["Signin and Signup", "Add and Update Profile", "Add and Update Product"],
+      "date": "March 30, 2016"
+    }
+  ]
+}
---
+++ js/featuresDataHandler.js
@@ -0,0 +1,41 @@
+jQuery(document).ready(function($){
+
+
+  $.getJSON('features.js').done(function (data) {
+
+    // Template loded from templates/features.htm with id timelineBlocks
+    $.get('../templates/features.htm', function(templates){
+      var template = $(templates).filter('#timelineBlocks').html();
+      $('#feature-container').append(Mustache.render(template, data));
+
+      // Animation function begin
+      var timelineBlocks = $('.cd-timeline-block'),
+    		offset = 0.8;
+
+    	//hide timeline blocks which are outside the viewport
+    	hideBlocks(timelineBlocks, offset);
+
+    	//on scolling, show/animate timeline blocks when enter the viewport
+    	$(window).on('scroll', function(){
+    		(!window.requestAnimationFrame)
+    			? setTimeout(function(){ showBlocks(timelineBlocks, offset); }, 100)
+    			: window.requestAnimationFrame(function(){ showBlocks(timelineBlocks, offset); });
+    	});
+
+    	function hideBlocks(blocks, offset) {
+    		blocks.each(function(){
+    			( $(this).offset().top > $(window).scrollTop()+$(window).height()*offset ) && $(this).find('.cd-timeline-img, .cd-timeline-content').addClass('is-hidden');
+    		});
+    	}
+
+    	function showBlocks(blocks, offset) {
+    		blocks.each(function(){
+    			( $(this).offset().top <= $(window).scrollTop()+$(window).height()*offset && $(this).find('.cd-timeline-img').hasClass('is-hidden') ) && $(this).find('.cd-timeline-img, .cd-timeline-content').removeClass('is-hidden').addClass('bounce-in');
+    		});
+    	}
+    })
+    // Animation functions ended
+
+  });
+
+});
